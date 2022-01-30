50e47d906519bf14f5836c6124440427ea9ff2d9
Kelvin Tran <kelvintran@Kelvins-Air.hitronhub.home>
2022-01-30 03:52:12 -0700

    added comments and cleaned up code

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -15,19 +15,23 @@ public class Parser {
        System.out.println(s);
     }
 
+    // parseJson takes a json file name and returns a string of unique file extensions and its count
     static String parseJSON(String jsonFile) throws FileNotFoundException{
-        //json schema
+        //loading in the schema file
         File schemaFile = new File("Data-schema.json");
         JSONTokener schemaData = new JSONTokener(new FileInputStream(schemaFile));
-        JSONObject jsonSchema = new JSONObject(schemaData);
 
-        //json data
+        //loading in the json file
+        JSONObject jsonSchema = new JSONObject(schemaData);
         File jsonData = new File(jsonFile);
         JSONTokener jsonDataFile = new JSONTokener(new FileInputStream(jsonData));
         JSONObject jsonObject;
+
+        //Creating a Schema which will be used to validate incomming data against the schema that we predefined in Data-schema.json
         Schema schemaValidator = SchemaLoader.load(jsonSchema);
+
         ArrayList<JSONObject> files = new ArrayList<JSONObject>();
-        LinkedHashMap<String, Set<String>> file_extensions = new LinkedHashMap<String, Set<String>>();
+        LinkedHashMap<String, Set<String>> file_extensions = new LinkedHashMap<String, Set<String>>(); //used to map unique file extensions to all of the unique files with the same extension
 
 
 
@@ -35,7 +39,7 @@ public class Parser {
             jsonObject = new JSONObject(jsonDataFile);
 
             try{
-                schemaValidator.validate(jsonObject);
+                schemaValidator.validate(jsonObject); //checking if the current data is valid based on the predefined schema
                 files.add(jsonObject);
 
                 formatFolderStructure(jsonObject);
@@ -52,7 +56,7 @@ public class Parser {
                 String file_extension = current_file[1];
 
 
-
+                // Constructing a the HashMap of file extensions and unique file
                 Set<String> s;
                 if(!file_extensions.containsKey(file_extension)){
                     s = new HashSet<String>();
@@ -60,15 +64,18 @@ public class Parser {
                 else{
                     s = file_extensions.get(file_extension);
                 }
-                s.add(ph);
+
+                s.add(ph); // HashSet does not allow duplicate entries so similar files will not be counted twice
                 file_extensions.put(file_extension, s);
 
             }
             catch (ValidationException e){
+                // prints the exception message whenever invalid data is caught by the schema validator
                 System.out.println(e);
             }
         }
 
+        //constructing a string to return
         String response = "";
         for(String key: file_extensions.keySet()){
             int size = file_extensions.get(key).size();
@@ -78,6 +85,8 @@ public class Parser {
     }
 
 
+    //validateFile takes in two strings, the file name (nm) and the file path (ph) and make sure that the files are consistent
+    //The file at the end of the path should have the same name as the included file (nm)
     static Boolean validateFile(String nm, String ph){
         String[] folder_structure = ph.split("/");
         String file_name = folder_structure[folder_structure.length-1];
@@ -86,6 +95,8 @@ public class Parser {
 
     }
 
+    // formatFolderStucture takes a JSONObject and formats its file path attribute to ensure all Objects are in the same format
+    // this method make sure the the file path leads and ends with forwards slashes and that the file name is included in the path
     static void formatFolderStructure(JSONObject jsonObject){
         String ph = jsonObject.getString("ph");
         String[] folder_structure = ph.split("/");
